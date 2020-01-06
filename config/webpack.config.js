4113d188b827c7fc08fbb76769cc3712dd5dfc0d
Elon Danziger <elon.danziger@globalstrategies.org>
2020-01-06 13:55:38 -0800

    Bugfix

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -401,7 +401,7 @@ module.exports = function(webpackEnv) {
             {
               text: /\.html$/,
               loader: require.resolve('html-loader'),
-            }
+            },
             // Process application JS with Babel.
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
